e3e647782890145bce292c1ffc7f1677ea9f19d6
AudreyHal <chiomahalim@gmail.com>
2020-04-28 00:04:44 +0100

    Add buttons to log in and out

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,7 +1,19 @@
 import React from "react"
+import Auth from "../utils/Auth";
+import { useHistory } from "react-router-dom";
+import { PublicPaths } from "../routes";
 
 const Dashboard=()=>{
-  return(<div>Dashboard</div>)
+  let history = useHistory();
+  const handleLogOut=()=>{
+    Auth.removeToken();
+    history.push(PublicPaths.LOGIN)
+  }
+
+  return(<div>
+    <h1>Dashboard</h1>
+     <button onClick={handleLogOut}> LOG OUT</button>
+  </div>)
 
 }
 
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React from "react";
+import Auth from "../utils/Auth";
+import { useHistory } from "react-router-dom";
+import { PrivatePaths } from "../routes";
 
 const Login=()=>{
-  return(<div>Login Page</div>)
+  let history = useHistory();
+  const handleLogIn=()=>{
+    Auth.setToken("exampleToken");
+    history.push(PrivatePaths.Dashboard)
+  }
+  return(<div>
+    <h1>Log In Page</h1>
+    <button onClick={handleLogIn}> LOG IN</button>
+  </div>)
 
 }
 
